2fbd544d059355824e33ac2308c2efd5a5019cca
IlyasOussama <zghariilyasoussama@gmail.com>
2022-10-20 00:22:30 +0100

    add classnames to the second button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -156,7 +156,12 @@ export default function App() {
             placeholder="pass in the eventID"
             className="styled-input"
           />
-          <button onClick={rsvpToEvent}>RSVP</button>
+          <button
+            className="py-2.5 px-5 mr-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+            onClick={rsvpToEvent}
+          >
+            RSVP
+          </button>
         </div>
         <div>
           {eventCreation && (
