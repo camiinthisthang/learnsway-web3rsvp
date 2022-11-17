4e615e93e1f1820d64c797962af185d052fc83d0
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2022-11-17 11:45:01 -0500

    add test, use snake case

--- rsvpContract/src/event_platform.sw
+++ rsvpContract/src/event_platform.sw
@@ -4,18 +4,18 @@ use std::{contract_id::ContractId, identity::Identity};
 
 abi eventPlatform {
     #[storage(read, write)]
-    fn create_event(maxCapacity: u64, deposit: u64, eventName: str[10]) -> Event;
+    fn create_event(max_capacity: u64, deposit: u64, event_name: str[10]) -> Event;
 
     #[storage(read, write)]
-    fn rsvp(eventId: u64) -> Event;
+    fn rsvp(event_id: u64) -> Event;
 }
 
 // defining the struct here because it would be used by other developers who would be importing this ABI
 pub struct Event {
-    uniqueId: u64,
-    maxCapacity: u64,
+    unique_id: u64,
+    max_capacity: u64,
     deposit: u64,
     owner: Identity,
     name: str[10],
-    numOfRSVPs: u64,
+    num_of_rsvps: u64,
 }
--- rsvpContract/tests/harness.rs
+++ rsvpContract/tests/harness.rs
@@ -12,6 +12,7 @@ async fn get_contract_instance() -> (MyContract, ContractId, Vec<WalletUnlocked>
             Some(1_000_000_000), /* Amount per coin */
         ),
         None,
+        None
     )
     .await;
     let wallet = wallets.pop().unwrap();
@@ -27,26 +28,83 @@ async fn get_contract_instance() -> (MyContract, ContractId, Vec<WalletUnlocked>
     .await
     .unwrap();
 
-    let instance = MyContractBuilder::new(id.to_string(), wallet).build();
+    let instance = MyContract::new(id.clone(), wallet);
 
     (instance, id.into(), wallets)
 }
 
 #[tokio::test]
 async fn can_get_contract_id() {
-    let (_instance, _id , wallets) = get_contract_instance().await;
-    
-    let wallet_1 = wallets.get(0).unwrap();
-    // println!("WALLET_1: {:?}", wallet_1);
-    let wallet_2 = wallets.get(1).unwrap();
-    // println!("WALLET_2: {:?}", wallet_2);
-    let wallet_3 = wallets.get(2).unwrap();
-    // println!("WALLET_3: {:?}", wallet_3);
-
-    let capacity: u64 = 10;
-    let price: u64 = 1;
-    let event_name: fuels::core::types::SizedAsciiString<10> = "abcde12345".try_into().expect("Should have succeeded");
-
+    let (instance, _id , wallets) = get_contract_instance().await;
 
     // Now you have an instance of your contract you can use to test each function
+    
+
+    // we can access some wallets for testing
+    let wallet_1 = wallets.get(0).unwrap();
+    let wallet_2 = wallets.get(1).unwrap();
+    // let wallet_3 = wallets.get(2).unwrap();
+
+    // check the initial balances for the wallets
+    let initial_balance_1: u64 = wallet_1.get_asset_balance(&BASE_ASSET_ID).await.unwrap();
+    let initial_balance_2: u64 = wallet_2.get_asset_balance(&BASE_ASSET_ID).await.unwrap();
+
+    println!("BALANCE 1: {:?}", initial_balance_1);
+    println!("BALANCE 2: {:?}", initial_balance_2);
+
+    // let's define some mock event details to use as paramenters for the create_event function
+    let capacity: u64 = 10;
+    let price: u64 = 25;
+    let event_name: SizedAsciiString<10> = "sway_party".try_into().expect("Should have succeeded");
+
+
+    // make the first event 
+    let project_1 = instance
+        .with_wallet(wallet_1.clone())
+        .unwrap()
+        .methods()
+        .create_event(capacity, price, event_name.clone())
+        .call()
+        .await
+        .unwrap();
+
+    println!("PROJECT 1: {:?}", project_1.value);
+    // make sure the project details are correct
+    assert!(project_1.value.max_capacity == capacity);
+    assert!(project_1.value.deposit == price);
+    assert!(project_1.value.name == event_name);
+
+       // Bytes representation of the asset ID of the "base" asset used for gas fees.
+       pub const BASE_ASSET_ID: AssetId = AssetId::new([0u8; 32]);
+
+       // call params to send the event price
+       let call_params = CallParameters::new(Some(price), Some(BASE_ASSET_ID), None);
+
+
+    // RSVP to the first project from wallet 2
+    let project_1_copy = instance
+        .with_wallet(wallet_2.clone())
+        .unwrap()
+        .methods()
+        .rsvp(0)
+        .append_variable_outputs(1)
+        .call_params(call_params)
+        .call()
+        .await
+        .unwrap();
+
+        println!("PROJECT 1 Copy: {:?}", project_1_copy.value);
+        //check if the num_of_rsvps is incremented
+        assert!(project_1_copy.value.num_of_rsvps == 1);
+
+        // check if the event owner received the price amount
+        let new_balance_1 = wallet_1.get_asset_balance(&BASE_ASSET_ID).await.unwrap();
+        let new_balance_2= wallet_2.get_asset_balance(&BASE_ASSET_ID).await.unwrap();
+
+        println!("BALANCE 1: {:?}", new_balance_1);
+        println!("BALANCE 2: {:?}", new_balance_2);
+
+        assert!(new_balance_1 == initial_balance_1 + price);
+        assert!(new_balance_2 == initial_balance_2 - price);
+
 }
